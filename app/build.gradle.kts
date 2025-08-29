f0f2123b79c2f9963e42b4bb6a1efe17bde56f68
Valery Karev <karev@34web.ru>
2025-08-29 22:56:14 +0300

    GH-2 Добавил базовые зависимости для выполнения проекта Добавил в gitignore папку /.idea/

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
+    id("com.google.devtools.ksp")
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -36,6 +38,7 @@ android {
 
     buildFeatures {
         buildConfig = true
+        compose = true
     }
 }
 
@@ -44,8 +47,30 @@ dependencies {
     implementation(libs.androidX.appCompat)
 
     // UI layer libraries
+    val composeBom = platform("androidx.compose:compose-bom:2025.08.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
     implementation(libs.ui.material)
     implementation(libs.ui.constraintLayout)
+    implementation(libs.material3)
+    implementation(libs.ui.tooling.preview)
+    debugImplementation(libs.ui.tooling)
+    implementation(libs.coil.compose)
+    implementation(libs.navigation.compose)
+
+
+    implementation(libs.koin)
+    implementation(libs.koinCompose)
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.coroutines)
+
+    implementation(libs.room.runtime)
+
+    ksp(libs.room.compiler)
+
+    implementation(libs.room.ktx)
 
     // region Unit tests
     testImplementation(libs.unitTests.junit)
@@ -55,4 +80,6 @@ dependencies {
     androidTestImplementation(libs.uiTests.junitExt)
     androidTestImplementation(libs.uiTests.espressoCore)
     // endregion
+
+
 }
