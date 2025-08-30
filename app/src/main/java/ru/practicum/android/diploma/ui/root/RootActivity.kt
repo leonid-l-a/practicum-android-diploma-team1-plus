f6a832fd7fe816740fbf30dfc439269073bbf1a4
Druzhkov Nikolay <diyavolik007@yandex.ru>
2025-08-30 17:54:39 +0300

    GH-8: реализованы темы и типография согласно макета 5.5

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -2,7 +2,6 @@ package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 
