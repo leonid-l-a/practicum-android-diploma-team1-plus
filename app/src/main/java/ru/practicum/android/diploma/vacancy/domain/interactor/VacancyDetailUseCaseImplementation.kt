5bdcd9a4b25fda54f3ee48c4bbb67e0a9640a4ac
Zdertis420 <AnRobo07@yandex.ru>
2025-09-04 18:54:11 +0300

    Небольшие фиксы по детекту

--- app/src/main/java/ru/practicum/android/diploma/vacancy/domain/interactor/VacancyDetailUseCaseImplementation.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/domain/interactor/VacancyDetailUseCaseImplementation.kt
@@ -3,7 +3,9 @@ package ru.practicum.android.diploma.vacancy.domain.interactor
 import ru.practicum.android.diploma.core.data.dto.VacancyDetail
 import ru.practicum.android.diploma.vacancy.domain.repository.VacancyDetailRepository
 
-class VacancyDetailUseCaseImplementation(private val vacancyDetailRepository: VacancyDetailRepository) : VacancyDetailUseCase {
+class VacancyDetailUseCaseImplementation(
+    private val vacancyDetailRepository: VacancyDetailRepository
+) : VacancyDetailUseCase {
     override suspend fun getVacancyDetail(id: String): VacancyDetail {
         return vacancyDetailRepository.getVacancyDetail(id).vacancyDetail
     }
