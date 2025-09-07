6a0236a3ca25775d0905ddb16d4b818018202987
Valery Karev <karev@34web.ru>
2025-09-07 16:58:33 +0300

    GH-30 Fix detekt

--- app/src/main/java/ru/practicum/android/diploma/main/ui/components/SearchBar.kt
+++ app/src/main/java/ru/practicum/android/diploma/main/ui/components/SearchBar.kt
@@ -1,14 +1,14 @@
 package ru.practicum.android.diploma.main.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
