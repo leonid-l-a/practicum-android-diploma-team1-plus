1c4177514ab499181d44fe5270d4042b8760be77
leonid-l-a <leo.lapaeff@gmail.com>
2025-09-08 15:06:55 +0200

    commit for 36th task: creating and setting for VacancyScreen

--- app/src/main/java/ru/practicum/android/diploma/core/navigation/AppNavHost.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/navigation/AppNavHost.kt
@@ -6,6 +6,11 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import org.koin.androidx.compose.koinViewModel
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import ru.practicum.android.diploma.vacancy.ui.components.VacancyScreen
+import ru.practicum.android.diploma.vacancy.ui.viewmodel.VacancyViewModel
 
 /**
  * The application's navigation host, managing navigation between screens.
@@ -18,9 +23,16 @@ fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) 
         // Заменить заглушки на реальные экраны
 
         composable(Screen.Main.route) { Text("Main Screen") }
+
         composable(Screen.Command.route) { Text("Command Screen") }
         composable(Screen.Favourites.route) { Text("Favourite Screen") }
-        composable(Screen.VacancyDetails.route) { Text("Vacancy Details Screen") }
+        composable(
+            route = Screen.VacancyDetails.route,
+            arguments = listOf(navArgument("vacancyId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val viewModel: VacancyViewModel = koinViewModel()
+            VacancyScreen(viewModel = viewModel,)
+        }
         composable(Screen.Filtration.route) { Text("Filtration Screen") }
         composable(Screen.Placement.route) { Text("Placement Screen") }
         composable(Screen.CountrySelection.route) { Text("Country Selection Screen") }
