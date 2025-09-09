b18fbd3b34ac1596b859926e6170d1b85a181681
leonid-l-a <leo.lapaeff@gmail.com>
2025-09-09 17:38:29 +0200

    GH54 VacancyScreen button onClicks setting

--- app/src/main/java/ru/practicum/android/diploma/vacancy/data/repository/ShareVacancyDetailRepositoryImplementation.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/data/repository/ShareVacancyDetailRepositoryImplementation.kt
@@ -1,19 +1,32 @@
 package ru.practicum.android.diploma.vacancy.data.repository
 
-import android.content.Context
 import android.content.Intent
+import androidx.core.net.toUri
 import ru.practicum.android.diploma.vacancy.domain.repository.ShareVacancyDetailRepository
 
-class ShareVacancyDetailRepositoryImplementation(private val context: Context) : ShareVacancyDetailRepository {
-    override fun shareVacancy(vacancyUrl: String): Intent {
+class ShareVacancyDetailRepositoryImplementation() : ShareVacancyDetailRepository {
+    override fun shareVacancyWithMessenger(vacancyUrl: String): Intent {
         return Intent.createChooser(Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(
                 Intent.EXTRA_TEXT,
                 vacancyUrl
             )
-            // Я вообще не уверен, что здесь нужно использовать text/plain, но пока надеюсь, что так правильно
             type = "text/plain"
         }, null)
     }
+
+    override fun writeWithMail(mail: String): Intent {
+        return Intent.createChooser(Intent(Intent.ACTION_SENDTO).apply {
+            data = "mailto:".toUri()
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(mail))
+        }, null)
+    }
+
+    override fun callWithPhone(phone: String): Intent {
+        return Intent.createChooser(Intent(Intent.ACTION_DIAL).apply {
+            data = "tel:$phone".toUri()
+        }, null)
+    }
+
 }
