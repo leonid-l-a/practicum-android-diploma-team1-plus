6e1bdfd8c0a5bbaf091704f49a60c8d24193b3cd
leonid-l-a <leo.lapaeff@gmail.com>
2025-09-09 18:36:15 +0200

    GH54 fix for showing bottomBar only on main screens

--- app/src/main/java/ru/practicum/android/diploma/core/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/ui/root/RootActivity.kt
@@ -5,22 +5,35 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import ru.practicum.android.diploma.core.navigation.AppNavHost
 import ru.practicum.android.diploma.core.navigation.BottomNavigationBar
+import ru.practicum.android.diploma.core.navigation.Screen
 import ru.practicum.android.diploma.core.ui.theme.ApplicationTheme
 
 class RootActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
+
+            val bottomBarRoutes = listOf(
+                Screen.Main.route,
+                Screen.Favorites.route,
+                Screen.Command.route
+            )
+
             ApplicationTheme {
                 Scaffold(
                     bottomBar = {
-                        BottomNavigationBar(navController)
+                        if (currentRoute in bottomBarRoutes) {
+                            BottomNavigationBar(navController)
+                        }
                     }
                 ) { padding ->
                     AppNavHost(
@@ -32,3 +45,4 @@ class RootActivity : AppCompatActivity() {
         }
     }
 }
+
