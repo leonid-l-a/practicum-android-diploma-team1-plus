3befc71c867331f46df96544fa6ff63ca9ccdede
Valery Karev <karev@34web.ru>
2025-09-12 10:52:07 +0300

    GH-87 Fix detekt

--- app/src/main/java/ru/practicum/android/diploma/core/ui/components/FilterItem.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/ui/components/FilterItem.kt
@@ -18,9 +18,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
 import android.content.res.Configuration
-
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.core.ui.components.model.FilterItem
 import ru.practicum.android.diploma.core.ui.theme.ApplicationTheme
