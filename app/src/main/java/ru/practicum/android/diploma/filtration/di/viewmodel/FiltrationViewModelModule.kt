63c3c957b394f073e62838ebcbfa43b2d0d29267
DruNik88 <135055202+DruNik88@users.noreply.github.com>
2025-09-14 22:23:16 +0300

    Update FiltrationViewModelModule.kt

--- app/src/main/java/ru/practicum/android/diploma/filtration/di/viewmodel/FiltrationViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/filtration/di/viewmodel/FiltrationViewModelModule.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.filtration.di.viewmodel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.filtration.ui.viewmodel.AreasViewModel
+import ru.practicum.android.diploma.filtration.ui.viewmodel.IndustriesViewModel
 
 val filtrationViewModelModule = module {
     viewModel { AreasViewModel(get()) }
