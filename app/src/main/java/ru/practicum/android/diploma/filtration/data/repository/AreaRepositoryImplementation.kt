7a9e0c86caad36368ecb177eb6baee176cccd426
leonid-l-a <leo.lapaeff@gmail.com>
2025-09-15 15:32:53 +0200

    GH-105 detekt fixes 2

--- app/src/main/java/ru/practicum/android/diploma/filtration/data/repository/AreaRepositoryImplementation.kt
+++ app/src/main/java/ru/practicum/android/diploma/filtration/data/repository/AreaRepositoryImplementation.kt
@@ -8,10 +8,12 @@ import ru.practicum.android.diploma.filtration.domain.state.Result
 class AreaRepositoryImplementation(
     private val vacancyNetworkClient: VacancyNetworkClient,
 ) : AreaRepository {
+    companion object {
+        private const val SUCCESS_RESPONSE_CODE = 200
+    }
     override suspend fun getAreas(): Result<FilterAreasResponse> {
-        val successResponseCode = 200
         val response = vacancyNetworkClient.getFilterAreas()
-        return if (response.resultCode == successResponseCode) {
+        return if (response.resultCode == SUCCESS_RESPONSE_CODE) {
             Result.Success(response)
         } else {
             Result.Error("Ошибка сети (код: ${response.resultCode})")
