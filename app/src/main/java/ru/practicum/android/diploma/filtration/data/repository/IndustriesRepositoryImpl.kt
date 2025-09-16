a151d8109c519c7c9731e211ad3d3627d469741c
Valery Karev <karev@34web.ru>
2025-09-16 07:23:27 +0300

    GH-76 Мучения

--- app/src/main/java/ru/practicum/android/diploma/filtration/data/repository/IndustriesRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filtration/data/repository/IndustriesRepositoryImpl.kt
@@ -7,7 +7,6 @@ import ru.practicum.android.diploma.core.data.network.VacancyNetworkClient
 import ru.practicum.android.diploma.core.data.utils.ResponseCode
 import ru.practicum.android.diploma.filtration.data.mapper.IndustriesMapper
 import ru.practicum.android.diploma.filtration.domain.model.Industries
-import ru.practicum.android.diploma.filtration.domain.model.IndustryDetail
 import ru.practicum.android.diploma.filtration.domain.model.StorageResultCode
 import ru.practicum.android.diploma.filtration.domain.repository.IndustriesRepository
 import ru.practicum.android.diploma.filtration.domain.state.Resource
@@ -18,29 +17,22 @@ class IndustriesRepositoryImpl(
     val networkUtil: NetworkUtil,
     val context: Context
 ) : IndustriesRepository {
-    private fun initIndustries(
-        typeError: StorageResultCode,
-        items: List<IndustryDetail> = emptyList(),
-    ): Industries {
-        return Industries(
-            items = items,
-            resultCode = StorageResultCode.SERVER_ERROR,
-        )
-    }
 
     override suspend fun getIndustries(): Flow<Resource<Industries>> {
         return flow {
             if (!networkUtil.isInternetAvailable(context)) {
                 emit(
                     Resource.Error(
-                        initIndustries(typeError = StorageResultCode.CLIENT_ERROR)
+                        Industries(
+                            items = emptyList(),
+                            resultCode = StorageResultCode.CLIENT_ERROR,
+                        )
                     )
                 )
             } else {
                 val response = networkClient.getFilterIndustries()
                 when (response.resultCode) {
                     ResponseCode.SUCCESS -> {
-                        val items =
                         emit(
                             Resource.Success(
                                 IndustriesMapper.toIndustries(
@@ -52,13 +44,19 @@ class IndustriesRepositoryImpl(
 
                     ResponseCode.IO_ERROR -> emit(
                         Resource.Error(
-                            initIndustries(typeError = StorageResultCode.SERVER_ERROR)
+                            Industries(
+                                items = emptyList(),
+                                resultCode = StorageResultCode.SERVER_ERROR,
+                            )
                         )
                     )
 
                     else -> emit(
                         Resource.Error(
-                            initIndustries(typeError = StorageResultCode.SERVER_ERROR)
+                            Industries(
+                                items = emptyList(),
+                                resultCode = StorageResultCode.SERVER_ERROR,
+                            )
                         )
                     )
                 }
